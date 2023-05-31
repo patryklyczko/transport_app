f3ea6fc15e1bdc506728d4de96003be95e0ce0fd
patryklyczko <lyczkopatryk1@gmail.com>
2023-05-31 21:39:27 +0200

    feat: scaler added

--- pkg/algorithms/minkowski_distance.go
+++ pkg/algorithms/minkowski_distance.go
@@ -3,7 +3,7 @@ package algorithms
 import "github.com/patryklyczko/transport_app/pkg/structures"
 
 func MinkowskiDistance(posStart, posEnd structures.Position) float32 {
-	return abs(posStart.Lat-posEnd.Lat) + abs(posStart.Lon-posEnd.Lon)
+	return abs(structures.LatScaler*(posStart.Lat-posEnd.Lat)) + abs(structures.LatScaler*(posStart.Lon-posEnd.Lon))
 }
 
 func abs(value float32) float32 {
